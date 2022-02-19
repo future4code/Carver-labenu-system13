da6b132ef6527cb4d9542bb304a84b11a6e9ebdb
Larissa Matos <larissamatos88@outlook.com>
2022-02-19 17:30:44 -0300

    Specialty criada e funcionando
    Co-Authored-By: Edson Magnini <32701426+edmagnini@users.noreply.github.com>

--- src/entities/capacities/teachersCapacities/teachersCapacities.ts
+++ src/entities/capacities/teachersCapacities/teachersCapacities.ts
@@ -3,10 +3,12 @@ import { UserCapacity } from "../userCapacity";
 export class TeachersCapacity extends UserCapacity {
 
     constructor(
+        id: string,
         userId: string,
         capacityId: string
     ) {
         super(
+            id,
             userId,
             capacityId
         )
